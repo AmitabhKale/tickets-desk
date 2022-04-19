21f7a758c17876a93e4231b0ab85448dc477bfb7
Amitabh Kale <amitabhkale@gmail.com>
2022-04-19 20:08:43 +0530

    Ticets CRUD functionality backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getTickets, createTicket} = require('../controllers/ticketController')
+const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 
 const router = express.Router();
 
@@ -7,6 +7,8 @@ const router = express.Router();
 const {protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router.route('/:id').get(protect, getTicket).put(protect, updateTicket).delete(protect, deleteTicket)
 
 
 module.exports = router;    
