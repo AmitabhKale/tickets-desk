4a9103f4cd82ab56406bd088db76464faa940719
Amitabh Kale <amitabhkale@gmail.com>
2022-04-19 19:18:47 +0530

    Added backend get and create ticket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -9,14 +9,44 @@ const Ticket = require('../models/ticketModel');
 // @route    /api/tickets
 // @access   Private
 const getTickets = asyncHandler( async (req,res) => {
-    res.status(200).json({ message: 'getTickets'})
+    const user = await User.findById(req.user.id);
+
+    if(!user){
+        res.status(401)
+        throw new Error('User Not Found')
+    }
+
+    const tickets = await Ticket.find({user: req.user.id})
+    res.status(200).json(tickets)
 })
 
 // @desc     Create new tickets
 // @route    /api/tickets
 // @access   Private
 const createTicket = asyncHandler( async (req,res) => {
-    res.status(200).json({ message: 'createTickets'})
+    const {product , description } = req.body;
+
+    if(!product || !description ){
+        res.status(400)
+        throw new Error('Please add a product and description')
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if(!user){
+        res.status(401)
+        throw new Error('User Not Found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+
+
+    res.status(201).json({ ticket})
 })
 
 module.exports = {
